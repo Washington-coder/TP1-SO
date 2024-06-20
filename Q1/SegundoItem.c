552ddef95126a7d1cc04c5377fed4f350458a588
washington <was.antonio.xy@gmail.com>
2024-06-20 18:12:55 -0400

    Q1 - SegundoItem usando semaforos

--- Q1/SegundoItem.c
+++ Q1/SegundoItem.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <unistd.h>
 
 #define NUM_LEITORES 5
 #define NUM_ESCRITORES 3
 
-int saldo = 1000;
+int saldo = 50;
 int leitores = 0;
 int escritores = 0;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_leitores = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond_escritores = PTHREAD_COND_INITIALIZER;
+sem_t sem_escritor;  // Semáforo para controlar o acesso dos escritores
+sem_t sem_leitor;  // Semáforo para controlar o acesso dos leitores
 
 void* leitor(void* arg) {
     int id = *((int*)arg);
     while (1) {
-        pthread_mutex_lock(&mutex);
-        
-        while (escritores > 0) {
-            pthread_cond_wait(&cond_leitores, &mutex);
-        }
+        sem_wait(&sem_leitor);
         leitores++;
         
-        pthread_mutex_unlock(&mutex);
+        if (leitores == 1){
+            sem_wait(&sem_escritor);
+        }
+
+        if (escritores == 0){
+            sem_post(&sem_leitor);
+        }
         
         printf("Leitor %d: Saldo atual é %d\n", id, saldo);
         
-        pthread_mutex_lock(&mutex);
+        sem_wait(&sem_leitor);
+        
         leitores--;
-        if (leitores == 0) {
-            pthread_cond_signal(&cond_escritores);
+        if (leitores == 0){
+            sem_post(&sem_escritor);
         }
-        pthread_mutex_unlock(&mutex);
+        
+        sem_post(&sem_leitor);
         
         sleep(1);
     }
@@ -43,22 +47,18 @@ void* leitor(void* arg) {
 void* escritor(void* arg) {
     int id = *((int*)arg);
     while (1) {
-        pthread_mutex_lock(&mutex);
-        
-        while (leitores > 0 || escritores > 0) {
-            pthread_cond_wait(&cond_escritores, &mutex);
-        }
+        sem_wait(&sem_escritor);
         escritores++;
         
-        pthread_mutex_unlock(&mutex);
-        
-        int operacao = rand() % 2;
-        int valor = rand() % 100;
+        int operacao = rand() % 2; // 0 para depósito, 1 para saque
+        int valor = 1;
         
         if (operacao == 0) {
+            // Deposito
             saldo += valor;
             printf("Escritor %d: Depósito de %d, Saldo atualizado é %d\n", id, valor, saldo);
         } else {
+            // Saque
             if (saldo >= valor) {
                 saldo -= valor;
                 printf("Escritor %d: Saque de %d, Saldo atualizado é %d\n", id, valor, saldo);
@@ -67,14 +67,8 @@ void* escritor(void* arg) {
             }
         }
         
-        pthread_mutex_lock(&mutex);
         escritores--;
-        if (escritores == 0) {
-            pthread_cond_broadcast(&cond_leitores);
-        } else {
-            pthread_cond_signal(&cond_escritores);
-        }
-        pthread_mutex_unlock(&mutex);
+        sem_post(&sem_escritor);
         
         sleep(1);
     }
@@ -87,6 +81,10 @@ int main() {
     int ids_leitores[NUM_LEITORES];
     int ids_escritores[NUM_ESCRITORES];
 
+    sem_init(&sem_escritor, 0, 1);
+    sem_init(&sem_leitor, 0, 1);
+
+    // Criação das threads leitoras
     for (int i = 0; i < NUM_LEITORES; i++) {
         ids_leitores[i] = i + 1;
         if (pthread_create(&leitores[i], NULL, leitor, &ids_leitores[i]) != 0) {
@@ -95,6 +93,7 @@ int main() {
         }
     }
 
+    // Criação das threads escritoras
     for (int i = 0; i < NUM_ESCRITORES; i++) {
         ids_escritores[i] = i + 1;
         if (pthread_create(&escritores[i], NULL, escritor, &ids_escritores[i]) != 0) {
@@ -109,6 +108,9 @@ int main() {
     for (int i = 0; i < NUM_ESCRITORES; i++) {
         pthread_join(escritores[i], NULL);
     }
+
+    sem_destroy(&sem_escritor);
+    sem_destroy(&sem_leitor);
 
     return 0;
 }
